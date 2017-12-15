cdf8e48640a2a3ed3481058636665172919a6e91
IMosafi <itay.mosafi@gmail.com>
2017-12-15 14:32:11 +0200

    1.2

---
+++ gen_examples.py
@@ -0,0 +1,104 @@
+import xeger
+import re
+import os
+from random import *
+
+LANGUAGE_REG_EXPRESSION = '[1-9]+a+[1-9]+b+[1-9]+c+[1-9]+d+[1-9]+'
+UPPER_LIMIT = 40
+NUM_VALID_EXAMPLES = 500
+NUM_INVALID_EXAMPLES = 500
+SIZE_OF_TRAIN = 30000
+SIZE_OF_TEST = 5000
+
+
+def generate_train_test_sequneces():
+    valid_sequences = generate_valid_examples((SIZE_OF_TRAIN + SIZE_OF_TEST) / 2)
+    invalid_sequences = generate_invalid_examples((SIZE_OF_TRAIN + SIZE_OF_TEST) / 2)
+    slicer = len(valid_sequences) - SIZE_OF_TEST / 2
+    train = valid_sequences[:slicer] + invalid_sequences[:slicer]
+    test = valid_sequences[slicer:] + invalid_sequences[slicer:]
+    return train, test
+
+
+def ensure_directory_exists(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+
+def get_regex_subitems():
+    items = []
+    items.append('[1-9]+')
+    items.append('a+')
+    items.append('[1-9]+')
+    items.append('b+')
+    items.append('[1-9]+')
+    items.append('c+')
+    items.append('[1-9]+')
+    items.append('d+')
+    items.append('[1-9]+')
+    return items
+
+
+def generate_invalid_sequence():
+    while True:
+        shuffle(regex_subitems)
+        new_regex = ''.join(regex_subitems)
+        sequence = str(xeger.xeger(new_regex))
+        if not is_valid_sequence(sequence):
+            break
+    return sequence
+
+
+def is_valid_sequence(sequence):
+    return re.sub(LANGUAGE_REG_EXPRESSION, '', sequence, count=1) == ''
+
+
+def generate_valid_examples(size):
+    sequences = []
+    for i in xrange(size):
+        xeger._limit = randint(1, UPPER_LIMIT)
+        sequences.append(str(xeger.xeger(LANGUAGE_REG_EXPRESSION)))
+    return sequences
+
+
+def generate_invalid_examples(size):
+    sequences = []
+    for i in xrange(size):
+        xeger._limit = randint(1, UPPER_LIMIT)
+        sequences.append(generate_invalid_sequence())
+    return sequences
+
+
+def test_valid_examples(examples):
+    assert (all(is_valid_sequence(x) for x in examples))
+    print 'test_valid_examples passed'
+
+
+def test_invalid_examples(examples):
+    assert (all(not is_valid_sequence(x) for x in examples))
+    print 'test_invalid_examples passed'
+
+
+if __name__ == '__main__':
+    xeger = xeger.Xeger(UPPER_LIMIT)
+    regex_subitems = get_regex_subitems()
+    valid_examples = generate_valid_examples(NUM_VALID_EXAMPLES)
+    test_valid_examples(valid_examples)
+    invalid_examples = generate_invalid_examples(NUM_INVALID_EXAMPLES)
+    test_invalid_examples(invalid_examples)
+
+    ensure_directory_exists('data/')
+
+    with open('data/pos_examples.txt', 'w') as f:
+        f.writelines([x + '\n' for x in valid_examples])
+
+    with open('data/neg_examples.txt', 'w') as f:
+        f.writelines([x + '\n' for x in invalid_examples])
+
+    train, test = generate_train_test_sequneces()
+
+    with open('data/train.txt', 'w') as f:
+        f.writelines([x + '\n' for x in train])
+
+    with open('data/test.txt', 'w') as f:
+        f.writelines([x + '\n' for x in test])
